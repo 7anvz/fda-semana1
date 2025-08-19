51ed36a8b439fb6709fa2de394a4a3b29c320864
7anvz <danjscommunity@gmail.com>
2025-08-19 11:42:44 -0500

    Mi primera subida

---
+++ Semana1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Subida a: Github");
+        }
+    }
+}
